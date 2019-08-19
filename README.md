474352ec45dbd01f9545cadbe64fe11934421625
Nik Clayton <nik@ngo.org.uk>
2019-08-20 00:20:46 +0200

    Implement the random-backend-selection proxy functionality
    Create a httputil.ReverseProxy director that randomly selects from a
    backend at runtime. Maintain a map from service domains to the
    reverse proxy for each domain and its backends.
    
    When handling a request check the "s" parameter for the service's
    domain, and proxy the request using the reverse proxy for that domain.
    
    Update the README accordingly.

--- README.md
+++ README.md
@@ -18,9 +18,39 @@ go build be/be.go
 
 It logs a representation of the parsed configuration and all of the address/port pairs it is listening on.
 
+## Build the load balancer / proxy
+
+The load balancer parses the configuration file and starts listening on the address and port defined in the `listen` section.
+
+In another shell,
+
+```shell
+go build lb/lb.go
+```
+
+## Run the load balancer / proxy
+
+Ordinarily the proxy would inspect the FQDN in the URL of the incoming request and match that against the `domain` in the configuration file to determine the list of backend hosts to proxy to.
+
+To simplify my development environment I've had the proxy honour an `s` parameter in the URL instead. The code that does this is clearly marked. Omitting the `s` parameter, or providing an invalid value returns a 404, with additional information logged.
+
+```shell
+./lb
+```
+
+## Other flags
+
+Both commands support a `--config` parameter to specify an alternate location for the configuration file.
+
+## Test in the browser
+
+The following assumes you haven't changed the default configuration. Adjust as necessasry.
+
+Visit `http://localhost:8080/?s=my-service.my-company.com`. The `lb` shell will log which of the two backends has been selected to proxy the request to, the `be` shell will show details of the received request, and the browser should show which of the two addresses was selected.
+
 # Requirements
 
-- [ ] Implement the proxy with a random-forwarding load balancing policy
+- [x] Implement the proxy with a random-forwarding load balancing policy
 
 - [ ] Provide a helm chart to deploy the proxy
 
--- lb/lb.go
+++ lb/lb.go
@@ -3,13 +3,16 @@ package main
 import (
 	"afe/config"
 	"flag"
-	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/http/httputil"
 )
 
 type Proxy struct {
 	config config.ProxyConfig
+	// reverseProxy maps a service domain to the ReverseProxy for that service
+	reverseProxy map[string]*httputil.ReverseProxy
 }
 
 var configPath = flag.String("config", "config.yaml", "full path to config file")
@@ -23,13 +26,55 @@ func main() {
 
 	log.Printf("config: %+v", proxy.config)
 
+	proxy.reverseProxy = make(map[string]*httputil.ReverseProxy)
+	for _, service := range proxy.config.Proxy.Services {
+		proxy.reverseProxy[service.Domain] = NewRandomBackendReverseProxy(service.Hosts)
+	}
+
 	http.HandleFunc("/", proxy.handler)
 	log.Fatal(http.ListenAndServe(proxy.config.Listen.String(), nil))
 }
 
+// handler implements the generic proxy.
+//
+// If this was a real application requests would be proxied based on
+// the domain of the target of the incoming request. Since I can't
+// trivially configure that, look for the first 's' parameter in the
+// URL query string and select the service and backends based on that.
 func (proxy Proxy) handler(w http.ResponseWriter, req *http.Request) {
-	_, err := io.WriteString(w, "hello, proxy\n")
-	if err != nil {
-		log.Fatal(err)
+	q := req.URL.Query()
+	service := q.Get("s")
+
+	if service == "" {
+		log.Printf("missing 's' parameter in URL %s\n", req.URL)
+		http.Error(w, "service not found", http.StatusNotFound)
+		return
 	}
+
+	backend, ok := proxy.reverseProxy[service]
+	if !ok {
+		log.Printf("no reverse proxy for s=%s\n", service)
+		http.Error(w, "service not found", http.StatusNotFound)
+		return
+	}
+
+	q.Del("s")
+	req.URL.RawQuery = q.Encode()
+	log.Printf("routing request for service %s\n", service)
+
+	backend.ServeHTTP(w, req)
+}
+
+// NewRandomBackendReverseProxy returns a new httputil.ReverseProxy which will
+// direct each request to a randomly selected backend.
+func NewRandomBackendReverseProxy(backends []config.HostPort) *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+		i := rand.Intn(len(backends))
+		backend := backends[i]
+		req.URL.Scheme = "http" // TODO: In real code this would be https
+		req.URL.Host = backend.String()
+		log.Printf("final URL: %s", req.URL)
+	}
+
+	return &httputil.ReverseProxy{Director: director}
 }
